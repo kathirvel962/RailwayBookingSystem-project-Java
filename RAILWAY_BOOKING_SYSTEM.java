6ce9b70279dffbcc0770a3455d90309e8593b506
Kathirvel_cse <kathirvel.s2024cse@sece.ac.in>
2025-09-15 10:32:13 +0530

    MAIN CLASS DECLARED

---
+++ RAILWAY_BOOKING_SYSTEM.java
@@ -0,0 +1,7 @@
+public class RAILWAY_BOOKING_SYSTEM {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Railway Booking System!");
+        // Additional code for the railway booking system would go here
+        
+    }
+}
